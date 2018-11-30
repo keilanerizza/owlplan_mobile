4884c8ad42709448a1ca4609e81e7adc51770fb0
Keilane Rizza <keilanerizza@gmail.com>
2018-11-29 22:27:45 -0200

    Criacao activity de form para criar turma

--- app/src/main/java/keilane/com/owlplan/MainActivity.java
+++ app/src/main/java/keilane/com/owlplan/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Toast.makeText(this,"opção de menu",Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, keilane.com.turmas.activities.ModalActivity.class));
             return true;
         }
 
---
+++ turmas/src/main/java/keilane/com/turmas/activities/ModalActivity.java
@@ -0,0 +1,78 @@
+package keilane.com.turmas.activities;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import keilane.com.turmas.R;
+
+public class ModalActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner spinner;
+    private static final String[] paths = {"item 1", "item 2", "item 3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_turma);
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                .createFromResource(this, R.array.brew_array,
+                        android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+        // onBackPressed();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    /*public void onBackPressed() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Do you want to logout?");
+        // alert.setMessage("Message");
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //Your action here
+            }
+        });
+
+        alert.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                });
+
+        alert.show();
+
+    }*/
+}
